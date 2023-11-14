fb2ed24941bdd37aedee47d739c7c55ea45191ee
Christopher Tran <tran.christopher09@gmail.com>
2023-11-14 22:49:02 +0000

    Added eventListener that changes image source to URL pasted in photo input #1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 // /* global data */
 
-// const $handlePhoto = document.querySelector('.image')
-// const $photoInput = document.querySelector('.photo')
+const $handlePhoto = document.querySelector('.image');
+const $photoInput = document.querySelector('.photo');
 
-// $photoInput.addEventListener()
+$photoInput.addEventListener('input', function (event) {
+  $handlePhoto.src = event.target.value;
+  console.log('event.target:', event.target);
+});
