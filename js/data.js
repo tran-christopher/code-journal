bca70d38ed02f3fa4aaa65dd18541f8f7c3c1851
Christopher Tran <tran.christopher09@gmail.com>
2023-11-15 02:19:54 +0000

    Working on beforeunload eventListener #1

--- js/data.js
+++ js/data.js
@@ -1,8 +1,12 @@
 /* exported data */
 
-let data = {
+const data = {
   view: 'entry-form',
   entries: [],
   editing: null,
   nextEntryId: 1,
 };
+
+window.addEventListener('beforeunload', function (event) {
+  JSON.stringify(data);
+});
--- js/main.js
+++ js/main.js
@@ -1,9 +1,26 @@
-// /* global data */
+/* global data */
 
-const $handlePhoto = document.querySelector('.image');
+const $titleInput = document.querySelector('.title');
+const $notesInput = document.querySelector('.notes');
 const $photoInput = document.querySelector('.photo');
+const $handlePhoto = document.querySelector('.image');
 
 $photoInput.addEventListener('input', function (event) {
   $handlePhoto.src = event.target.value;
   console.log('event.target:', event.target);
+});
+
+const $handleForm = document.querySelector('.form');
+
+$handleForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+  data.entries.push({
+    title: $titleInput.value,
+    url: $photoInput.value,
+    notes: $notesInput.value,
+    entryId: data.nextEntryId,
+  });
+  data.nextEntryId++;
+  $handlePhoto.src = 'images/placeholder-image-square.jpg';
+  $handleForm.reset();
 });
