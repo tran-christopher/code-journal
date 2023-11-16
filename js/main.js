12fc86c5797f13eacd16c4824b85c1b51b2241e6
Christopher Tran <tran.christopher09@gmail.com>
2023-11-16 18:00:58 +0000

    Defined function renderEntry and eventListener for DOMContentLoaded event, testing now #2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 /* global data */
 
+// Issue #1
 const $titleInput = document.querySelector('.title');
 const $notesInput = document.querySelector('.notes');
 const $photoInput = document.querySelector('.photo');
@@ -22,4 +23,40 @@ $handleForm.addEventListener('submit', function (event) {
   data.nextEntryId = data.nextEntryId + 1;
   $handlePhoto.src = 'images/placeholder-image-square.jpg';
   $handleForm.reset();
+});
+
+// Issue #2
+function renderEntry(entry) {
+  const $li = document.createElement('li');
+  const $imageDiv = document.createElement('div');
+  const $image = document.createElement('img');
+  const $textDiv = document.createElement('div');
+  const $h3 = document.createElement('h3');
+  const $p = document.createElement('p');
+
+  $li.setAttribute('class', 'row');
+  $imageDiv.setAttribute('class', 'column-half');
+  $image.setAttribute('src', $photoInput.value);
+  $image.setAttribute('alt', 'Fix your link!');
+  $textDiv.setAttribute('class', 'column-half');
+
+  $h3.textContent = $titleInput.value;
+  $p.textContent = $notesInput.value;
+
+  $li.appendChild($imageDiv);
+  $li.appendChild($textDiv);
+  $imageDiv.appendChild($image);
+  $textDiv.appendChild($h3);
+  $textDiv.appendChild($p);
+
+  return $li;
+}
+
+const $list = document.querySelector('.no-bullets');
+
+document.addEventListener('DOMContentLoaded', function (event) {
+  for (let i = 0; i < data.entries.length; i++) {
+    const $newEntry = renderEntry(data.entries[i]);
+    $list.appendChild($newEntry);
+  }
 });
